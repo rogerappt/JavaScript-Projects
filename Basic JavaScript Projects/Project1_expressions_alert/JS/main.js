ee58b8405ed56e3eda7d8dfad396ab5581f082eb
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-18 20:12:17 +0000

    Assignment Submission 1

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,15 +1,24 @@
-//var B = ("\"What is JavaScript? "
-//+ "-JavaScript is a scripting language that enables you to create dynamically updating content;"
-//+ "-Control multimedia, animate images;" 
-//+ "-And pretty much everything else.\"");
-// document.write (B)
-//var blues = ("Name = Marius, Age = 37, + Email = me@test.com, + Address = myHome");
-//var blues = blues.fontcolor("blue");
-//document.write (blues);
-//100 + 37
-//document.write(100 + 37)
-function My_First_Function () {                               //Defining a function and naming it
-    var str = "This text is green!";                          //Defining a variable and giving it a string value
-    var result = str.fontcolor("green");                      //Using the fontcolor method on str variable
-    document.getElementById("Green_Text").innerHTML = result; //Putting the value of result into HTML element with "Green_Text" id
+//Use the window.alert() and document.write() methods.
+window.alert ("Helo World");
+document.write("Hello World"+"<br>");   
+//Assign string values to two variables and concatenate the variables together.
+function myFunction () {
+    var sentence = "I am learning";
+    var sentence2 = "a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence+sentence2;    //I Concatonate 2 sentences.
 }
+//Assign a color to at least one variable using the fontcolor() method.
+var B = ("\"What is JavaScript? "
++ "-JavaScript is a scripting language that enables you to create dynamically updating content;"
++ "-Control multimedia, animate images;" 
++ "-And pretty much everything else.\""+"<br>");
+ document.write (B);
+var blues = ("Name = Marius, Age = 37, + Email = me@test.com, + Address = myHome");
+var blues = blues.fontcolor("blue");   //Fontcolor it is used now style.color/ Here I use text color.
+document.write (blues+"<br>");
+//Write an expression.
+document.write(100 + 37);     // This is an expression
+//Add comments throughout your JavaScript explaining your code.
+
+
+
