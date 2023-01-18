af0fe9247eb6b52af1949906a847cabf98b40408
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-18 20:31:31 +0000

    Assignment Submission 2
    •  Write a function that uses the += operator
    •  Create an HTML element to call and display your function
    •  Add comments throughout your JavaScript explaining your code

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
-function Assignment_13 () {
-    var str = "This is Assignment 13";
+function Assignment_Submission_2 () {
+    var str = "Assignment Submission 2";
     var result = str.fontcolor("red");
-    document.getElementById("Red_Text").innerHTML = result;
+    document.getElementById("Red_Text").innerHTML = result;     
 }
 function myFunction () {
     var sentence = "I am learning";
     sentence += "a lot from this course!";
-    document.getElementById("Concatenate").innerHTML = sentence; 
+    document.getElementById("Concatenate").innerHTML = sentence;     //I write a function that used += operator
 }
