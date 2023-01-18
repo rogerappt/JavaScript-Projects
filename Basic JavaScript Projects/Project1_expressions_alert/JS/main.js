d6ad3c358d1d91c2ac8a1a0b4146f8b93999b212
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-18 15:59:26 +0000

    New start

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+//var B = ("\"What is JavaScript? "
+//+ "-JavaScript is a scripting language that enables you to create dynamically updating content;"
+//+ "-Control multimedia, animate images;" 
+//+ "-And pretty much everything else.\"");
+// document.write (B)
+//var blues = ("Name = Marius, Age = 37, + Email = me@test.com, + Address = myHome");
+//var blues = blues.fontcolor("blue");
+//document.write (blues);
+100 + 37
+document.write(100 + 37)
