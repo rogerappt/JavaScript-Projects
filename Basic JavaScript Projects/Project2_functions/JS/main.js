c3246bab647507f1953f9f7772e6b1ffb1abf573
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-18 18:54:44 +0000

    Late1

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,8 @@ function Assignment_13 () {
     var result = str.fontcolor("red");
     document.getElementById("Red_Text").innerHTML = result;
 }
+function myFunction () {
+    var sentence = "I am learning";
+    sentence += "a lot from this course!";
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
