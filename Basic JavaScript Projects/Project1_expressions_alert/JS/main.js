6bf1c924a9197cf64fa6b064796bf4d880906e2b
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-18 18:24:35 +0000

    On Assignment 13

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -6,5 +6,10 @@
 //var blues = ("Name = Marius, Age = 37, + Email = me@test.com, + Address = myHome");
 //var blues = blues.fontcolor("blue");
 //document.write (blues);
-100 + 37
-document.write(100 + 37)
+//100 + 37
+//document.write(100 + 37)
+function My_First_Function () {                               //Defining a function and naming it
+    var str = "This text is green!";                          //Defining a variable and giving it a string value
+    var result = str.fontcolor("green");                      //Using the fontcolor method on str variable
+    document.getElementById("Green_Text").innerHTML = result; //Putting the value of result into HTML element with "Green_Text" id
+}
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Assignment_13 () {
+    var str = "This is Assignment 13";
+    var result = str.fontcolor("red");
+    document.getElementById("Red_Text").innerHTML = result;
+}
