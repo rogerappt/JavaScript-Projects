9e2decb64ef07eb6b95a65b936f3c30ec3f6754d
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-22 20:07:38 +0000

    Assignment Submission 5
    Use the document.write() method and typeof operator to display the data type of a variable of your choosing
    •  Write an expression combining a string and a number
    •  Write code using the following operators: ==, ===, >, <, &&, ||, !  and display them in the browser
    •  Add comments throughout your JavaScript explaining your code

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/Example.js
@@ -0,0 +1,5 @@
+function my_Function () {
+    document.getElementById("Test") . innerHTML = 0/0;
+    document.getElementById("Test1") . innerHTML = isNaN ("This is a string");
+    document.getElementById("Test2") . innerHTML = isNaN ("007");
+}
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,60 @@
+//Display the data type of a variable of your choosing
+document.write(typeof 3 + "<br>"); //Return the keyword number
+
+document.write(3E310 + "<br>");    //Return Infinit
+document.write(-3E310 + "<br>");   //Return -Infinit
+document.write(10 < 2);            //Return False in Console       //!!!!!!Need to add <br> here
+document.write("<br>");
+document.write(10 > 2);            //Return True in Console        //!!!!!!Need to add <br> here
+document.write("<br>");
+console.log(2 + 2);
+document.write("10" + 5);          //Coercion / Return 105
+document.write("<br>");
+console.log(10 < 2);               //Boolean
+document.write("<br>");
+
+//Write code using the following operators: ==, ===, >, <, &&, ||, !  and display them in the browser
+document.write(10 == 10);
+document.write("<br>");
+document.write(3 == 11);
+document.write("<br>");
+//Return true by ensuring to match the data type and value.
+
+X = 10;
+Y = 10;
+document.write(X === Y);
+document.write("<br>");
+//Return false by writing a different data type and different value.
+
+X = "10";
+Y = 11;
+document.write(X === Y);
+document.write("<br>");
+//Return false by writing a different data type but the same value for both.
+X = "10";
+Y = 10;
+document.write(X === Y);
+document.write("<br>");
+//Return false by writing the same data type but a different value for both.
+X = 10;
+Y = 11;
+document.write(X === Y);
+document.write("<br>");
+document.write(5 > 2 && 10 > 4);    //AND operator display true 
+document.write("<br>");
+document.write(5 > 2 && 10 < 4);    //AND operator display false when one of two are false
+document.write("<br>");
+document.write(5 > 10 && 10 > 4);   //AND operator display truebecause, while 5 is not greater than 10, 10 is greater than 4
+document.write("<br>");
+document.write(5 > 10 && 10 > 20);  //AND operator display false
+document.write("<br>");
+function not_Function() {           //The ! (not) operator checks whether or not something is true. If ___ is false, “true” will be returned.
+    document.getElementById("Not").innerHTML = !(5 > 10);    //This would return “True”.
+}
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);    //This would return “False”.
+}
+
+//Write an expression combining a string and a number
+let myAge = 85;
+console.log("I am " + myAge + " years old.");   // Return I am 85 years old.
