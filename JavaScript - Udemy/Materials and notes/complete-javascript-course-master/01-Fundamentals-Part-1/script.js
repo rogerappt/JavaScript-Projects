0086b9a3652999fdd6245f831e655efe9fc9d7a4
rogerappt <100250348+rogerappt@users.noreply.github.com>
2023-01-24 20:28:17 +0000

    Add new repository
    JavaScript - Udemy

---
+++ JavaScript - Udemy/Materials and notes/complete-javascript-course-master/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,24 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';    //Un mod recomandat sa scriem - Este un mod in care variabila este descriptiva
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';          //Un mod nu tocmai recomandat sa scriem - Este un mod in care variabile nu este foarte descriptiva
+let job2 = 'teacher';
+
+console.log(myFirstJob);
